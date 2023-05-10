db3b8b025813c1e4b4182ba50a9e47d6896b82ba
piyopiyo_dev <86957750+PiyoPiyo-dev@users.noreply.github.com>
2023-05-10 22:04:21 +0900

    Update TohyoTalk.py

--- TohyoTalk.py
+++ TohyoTalk.py
@@ -8,7 +8,7 @@ from io import BytesIO
 
 
 class TohyoTalk:
-    def __init__(self):
+    def __init__(self, debug=False):
         self.headers = {
             'authority': 'tohyotalk.com',
             'accept': 'application/json, text/javascript, */*; q=0.01',
@@ -23,17 +23,26 @@ class TohyoTalk:
             'user-agent': 'Mozilla/5.0 (iPad; CPU OS 13_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1 Edg/112.0.0.0',
             'x-requested-with': 'XMLHttpRequest',
         }
+        self.debug = debug
         self.data = None
 
-    def fueldid(self):
-        r = requests.get('https://tohyotalk.com/')
+    def get_fueldid(self, proxy=None):
+        proxies = {
+            "http": proxy,
+            "https": proxy,
+        }
+        r = requests.get('https://tohyotalk.com/', proxies=proxies)
         return r.cookies.get('fueldid')
 
-    def create_account(self, userId, userName, password="password"):
-        fueldid = self.fueldid()
+    def create_account(self, userId, userName, password="password", proxy=None):
+        self.fueldid = self.get_fueldid(proxy)
+        proxies = {
+            "http": proxy,
+            "https": proxy,
+        }
         cookies = {
             'sf': '4',
-            'fueldid': fueldid
+            'fueldid': self.fueldid
         }
         data = {
             'tt_id': userId,
@@ -42,7 +51,9 @@ class TohyoTalk:
         }
 
         r = requests.post('https://tohyotalk.com/api/updateaccount.json',
-                          cookies=cookies, headers=self.headers, data=data)
+                          cookies=cookies, headers=self.headers, data=data, proxies=proxies)
+        if self.debug:
+            print(r.text)
         if json.loads(r.text)["status"] == 0:
             user_id = r.cookies.get("user_id")
             phpsessid = r.cookies.get("PHPSESSID")
@@ -52,13 +63,13 @@ class TohyoTalk:
                 'PHPSESSID': phpsessid,
                 'user_id': user_id,
                 'd4': d4,
-                'fueldid': fueldid}
+                'fueldid': self.fueldid}
             data = {
                 'tt_id': userId,
                 'tt_pw': password,
             }
             r = requests.post('https://tohyotalk.com/api/updatepassword.json',
-                              cookies=cookies, headers=self.headers, data=data)
+                              cookies=cookies, headers=self.headers, data=data, proxies=proxies)
             self.data = {
                 "PHPSESSID": phpsessid,
                 "d4": d4,
@@ -66,6 +77,13 @@ class TohyoTalk:
                 "userId": userId,
                 "password": password
             }
+            if self.debug:
+                print({
+                    "status": True,
+                    "userName": userName,
+                    "userId": userId,
+                    "password": password
+                })
             return {
                 "status": True,
                 "userName": userName,
@@ -74,22 +92,32 @@ class TohyoTalk:
             }
 
         else:
+            if self.debug:
+                print({
+                    "status": False,
+                })
             return {
                 "status": False
             }
 
-    def login(self, userId, password):
-        fueldid = self.fueldid()
+    def login(self, userId, password, proxy=None):
+        self.fueldid = self.get_fueldid(proxy)
+        proxies = {
+            "http": proxy,
+            "https": proxy,
+        }
         cookies = {
             'sf': '4',
-            'fueldid': fueldid
+            'fueldid': self.fueldid
         }
         data = {
             'tt_id': userId,
             'tt_pw': password,
         }
         r = requests.post('https://tohyotalk.com/api/idrenkei.json',
-                          cookies=cookies, headers=self.headers, data=data)
+                          cookies=cookies, headers=self.headers, data=data, proxies=proxies)
+        if self.debug:
+            print(r.text)
         if json.loads(r.text)["status"] == 0:
             self.data = {
                 "PHPSESSID": r.cookies.get("PHPSESSID"),
@@ -98,17 +126,29 @@ class TohyoTalk:
                 "userId": userId,
                 "password": password
             }
+            if self.debug:
+                print({
+                    "status": True,
+                })
             return {
                 "status": True,
             }
         else:
+            if self.debug:
+                print({
+                    "status": False,
+                })
             return {
                 "status": False,
             }
 
-    def vote(self, vote_id, target):
+    def vote(self, vote_id, target, proxy=None):
         if not self.data == None:
-            fueldid = self.fueldid()
+            fueldid = self.fueldid
+            proxies = {
+                "http": proxy,
+                "https": proxy,
+            }
             cookies = {
                 'sf': '4',
                 'PHPSESSID': self.data["PHPSESSID"],
@@ -121,23 +161,41 @@ class TohyoTalk:
                 'target': target,
             }
             r = requests.post('https://tohyotalk.com/api/vote.json',
-                              cookies=cookies, headers=self.headers, data=data)
+                              cookies=cookies, headers=self.headers, data=data, proxies=proxies)
+            if self.debug:
+                print(r.text)
             if json.loads(r.text)["status"] == 0:
+                if self.debug:
+                    print({
+                        "status": True
+                    })
                 return {
                     "status": True
                 }
             else:
+                if self.debug:
+                    print({
+                        "status": False
+                    })
                 return {
                     "status": False
                 }
         else:
+            if self.debug:
+                print({
+                    "status": False
+                })
             return {
                 "status": False
             }
 
-    def tweet(self, content):
+    def tweet(self, content, proxy=None):
         if not self.data == None:
-            fueldid = self.fueldid()
+            fueldid = self.fueldid
+            proxies = {
+                "http": proxy,
+                "https": proxy,
+            }
             cookies = {
                 'sf': '4',
                 'PHPSESSID': self.data["PHPSESSID"],
@@ -150,24 +208,43 @@ class TohyoTalk:
                 'content': content,
             }
             r = requests.post('https://tohyotalk.com/api/createmsg2.json',
-                              cookies=cookies, headers=self.headers, data=data)
+                              cookies=cookies, headers=self.headers, data=data, proxies=proxies)
+            if self.debug:
+                print(r.text)
             if json.loads(r.text)["status"] == 0:
+                if self.debug:
+                    print({
+                        "status": True,
+                        "msgId": json.loads(r.text)["msgId"]
+                    })
                 return {
                     "status": True,
                     "msgId": json.loads(r.text)["msgId"]
                 }
             else:
+                if self.debug:
+                    print({
+                        "status": False
+                    })
                 return {
                     "status": False
                 }
         else:
+            if self.debug:
+                print({
+                    "status": False
+                })
             return {
                 "status": False
             }
 
-    def follow(self, id):
+    def follow(self, id, proxy=None):
         if not self.data == None:
-            fueldid = self.fueldid()
+            fueldid = self.fueldid
+            proxies = {
+                "http": proxy,
+                "https": proxy,
+            }
             cookies = {
                 'sf': '4',
                 'PHPSESSID': self.data["PHPSESSID"],
@@ -180,23 +257,41 @@ class TohyoTalk:
                 'followed': "",
             }
             r = requests.post('https://tohyotalk.com/api/saveuserfollowuser.json',
-                              cookies=cookies, headers=self.headers, data=data)
+                              cookies=cookies, headers=self.headers, data=data, proxies=proxies)
+            if self.debug:
+                print(r.text)
             if json.loads(r.text)["status"] == 0:
+                if self.debug:
+                    print({
+                        "status": True
+                    })
                 return {
                     "status": True
                 }
             else:
+                if self.debug:
+                    print({
+                        "status": False
+                    })
                 return {
                     "status": False
                 }
         else:
+            if self.debug:
+                print({
+                    "status": False
+                })
             return {
                 "status": False
             }
 
-    def like(self, msgId):
+    def like(self, msgId, proxy=None):
         if not self.data == None:
-            fueldid = self.fueldid()
+            fueldid = self.get_fueldid(proxy)
+            proxies = {
+                "http": proxy,
+                "https": proxy,
+            }
             cookies = {
                 'sf': '4',
                 'PHPSESSID': self.data["PHPSESSID"],
@@ -209,23 +304,41 @@ class TohyoTalk:
                 'eval_mode': 'plus',
             }
             r = requests.post('https://tohyotalk.com/api/evalmsg.json',
-                              cookies=cookies, headers=self.headers, data=data)
+                              cookies=cookies, headers=self.headers, data=data, proxies=proxies)
+            if self.debug:
+                print(r.text)
             if json.loads(r.text)["status"] == 0:
+                if self.debug:
+                    print({
+                        "status": True
+                    })
                 return {
                     "status": True
                 }
             else:
+                if self.debug:
+                    print({
+                        "status": False
+                    })
                 return {
                     "status": False
                 }
         else:
+            if self.debug:
+                print({
+                    "status": False
+                })
             return {
                 "status": False
             }
 
-    def game(self, score):
+    def game(self, score, proxy=None):
         if not self.data == None:
-            fueldid = self.fueldid()
+            fueldid = self.fueldid
+            proxies = {
+                "http": proxy,
+                "https": proxy,
+            }
             cookies = {
                 'sf': '4',
                 'PHPSESSID': self.data["PHPSESSID"],
@@ -240,25 +353,43 @@ class TohyoTalk:
                 'play_id': '',
             }
             r = requests.post('https://tohyotalk.com/api/saveusergamescore.json',
-                              cookies=cookies, headers=self.headers, data=data)
+                              cookies=cookies, headers=self.headers, data=data, proxies=proxies)
+            if self.debug:
+                print(r.text)
             if json.loads(r.text)["status"] == 0:
+                if self.debug:
+                    print({
+                        "status": True,
+                        "rank": json.loads(r.text)["this_rank"]
+                    })
                 return {
                     "status": True,
                     "rank": json.loads(r.text)["this_rank"]
-
                 }
             else:
+                if self.debug:
+                    print({
+                        "status": False
+                    })
                 return {
                     "status": False
                 }
         else:
+            if self.debug:
+                print({
+                    "status": False
+                })
             return {
                 "status": False
             }
 
-    def icon(self, icon_path):
+    def icon(self, icon_path, proxy=None):
         if not self.data == None:
-            fueldid = self.fueldid()
+            fueldid = self.fueldid
+            proxies = {
+                "http": proxy,
+                "https": proxy,
+            }
             cookies = {
                 'sf': '4',
                 'PHPSESSID': self.data["PHPSESSID"],
@@ -289,7 +420,7 @@ class TohyoTalk:
                 'x-requested-with': 'XMLHttpRequest',
             }
             r = requests.post('https://tohyotalk.com/api/uploadimg.json',
-                              cookies=cookies, headers=headers, data=data
+                              cookies=cookies, headers=headers, data=data, proxies=proxies
                               )
             if json.loads(r.text)["status"] == 0:
                 x, y = Image.open(BytesIO(requests.get(
@@ -303,27 +434,47 @@ class TohyoTalk:
                     'y2': y,
                 }
                 r = requests.post('https://tohyotalk.com/api/cropimg.json',
-                                  cookies=cookies, headers=self.headers, data=data)
+                                  cookies=cookies, headers=self.headers, data=data, proxies=proxies)
                 if json.loads(r.text)["status"] == 0:
+                    if self.debug:
+                        print({
+                            "status": True
+                        })
                     return {
                         "status": True
                     }
                 else:
+                    if self.debug:
+                        print({
+                            "status": False
+                        })
                     return {
                         "status": False
                     }
             else:
+                if self.debug:
+                    print({
+                        "status": False
+                    })
                 return {
                     "status": False
                 }
         else:
+            if self.debug:
+                print({
+                    "status": False
+                })
             return {
                 "status": False
             }
 
-    def profile(self, content):
+    def profile(self, content, proxy=None):
         if not self.data == None:
-            fueldid = self.fueldid()
+            fueldid = self.fueldid
+            proxies = {
+                "http": proxy,
+                "https": proxy,
+            }
             cookies = {
                 'sf': '4',
                 'PHPSESSID': self.data["PHPSESSID"],
@@ -336,16 +487,30 @@ class TohyoTalk:
                 'content': content
             }
             r = requests.post('https://tohyotalk.com/api/saveprofilecontent.json',
-                              cookies=cookies, headers=self.headers, data=data)
+                              cookies=cookies, headers=self.headers, data=data, proxies=proxies)
+            if self.debug:
+                print(r.text)
             if json.loads(r.text)["status"] == 0:
+                if self.debug:
+                    print({
+                        "status": True
+                    })
                 return {
                     "status": True
                 }
             else:
+                if self.debug:
+                    print({
+                        "status": False
+                    })
                 return {
                     "status": False
                 }
         else:
+            if self.debug:
+                print({
+                    "status": False
+                })
             return {
                 "status": False
             }
